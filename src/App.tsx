b7de12b8b1cae70fe6d8c554e37a6e8119e690d2
sac6inarya <sachinkarya333@gmail.com>
2023-01-27 15:16:32 +0530

    Switch done

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,26 @@
 import "./App.css";
-import Radio, { RadioFieldProps } from "./components/Radio/index";
-import { Option } from "./components/Radio/index";
 import { Formik } from "formik";
-import * as Yup from "yup";
-// import { ReactForm } from "react-forms";
+import Switch, { SwitchFieldProps } from "./components/Checkbox/Switch";
 
-const genderOptions: Option[] = [
-  { value: "male", label: "Male" },
-  { value: "female", label: "Female" },
-  { value: "other", label: "Other" },
-];
-const genderFieldProps: RadioFieldProps = {
-  name: "gender",
-  options: genderOptions,
-  header: "Select Gender",
-  helperText: "Select any one option",
-  column: true,
-};
-
-const placedOptions: Option[] = [
-  { value: "yes", label: "Yes" },
-  { value: "no", label: "No" },
-];
-const placedFieldProps: RadioFieldProps = {
-  name: "placed",
-  options: placedOptions,
-  header: "Placed?",
-  helperText: "Select any one option",
+const onOrOff: SwitchFieldProps = {
+  name: "switchValue",
+  label: "Switch",
+  helperText: "Off On",
 };
 
 function App() {
-  const validationSchema = Yup.object({
-    gender: Yup.string().required("Required"),
-    placed: Yup.string().required("Required"),
-  });
-
-  const initialValues = {
-    gender: "",
-    placed: "",
-  };
-
   return (
     <div className="App">
       <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
+        initialValues={{}}
         onSubmit={(values) => {
           console.log(values);
         }}
       >
         {(formikProps) => {
           return (
-            <form onSubmit={formikProps.handleSubmit} className="radio-form">
-              <Radio formikProps={formikProps} fieldProps={genderFieldProps} />
-              <Radio formikProps={formikProps} fieldProps={placedFieldProps} />
-              <button type="submit">Submit</button>
+            <form>
+              <Switch formikProps={formikProps} fieldProps={onOrOff} />
             </form>
           );
         }}
---
+++ src/components/Checkbox/Switch/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FieldProps } from "../../../Types";
+import "./styles.css";
+import { get } from "lodash";
+
+export interface SwitchFieldProps {
+  name?: string;
+  label?: string;
+  helperText?: string;
+}
+
+interface SwithcProps extends FieldProps {
+  fieldProps: SwitchFieldProps;
+}
+
+const Switch: React.FC<SwithcProps> = ({ formikProps, fieldProps }) => {
+  const { label, name, helperText } = fieldProps;
+
+  const fieldValue = get(formikProps, `values.${name}`);
+  const handleOnChange = () => {
+    formikProps.setFieldValue(`${name}`, !fieldValue);
+  };
+  return (
+    <div className="switch-field">
+      <span className="switch-label">{label}</span>
+      <label className="switch-container">
+        <input
+          className="slider"
+          type="checkbox"
+          checked={!!fieldValue}
+          value={fieldValue}
+          onChange={handleOnChange}
+        />
+        <span className="slider round"></span>
+      </label>
+      <span className="helper-text">{helperText}</span>
+    </div>
+  );
+};
+
+export default Switch;
